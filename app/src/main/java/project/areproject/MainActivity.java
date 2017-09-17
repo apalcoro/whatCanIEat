c691643ab93949da6162f0de596532d28863fa65
Almog_Shemesh <almog_52@walla.com>
2017-09-17 15:36:01 +0300

    Initial files

---
+++ app/src/main/java/project/areproject/MainActivity.java
@@ -0,0 +1,30 @@
+package project.areproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button register;
+    Button manual;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        register = (Button)findViewById(R.id.register);
+        manual = (Button)findViewById(R.id.manual);
+        login = (Button)findViewById(R.id.login);
+    }
+
+    public void Register(View view) {
+
+    }
+
+    public void Login(View view) {
+    }
+
+    public void MoveManual(View view) {
+    }
+}
